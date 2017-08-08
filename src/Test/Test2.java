ba8795aae60abbe2ccede4fe2fb24c3a10b577ac
ABDULRAOOF003 <ABDULRAOOF003@YAHOO.CO.UK>
2017-08-08 11:04:46 +0100

    second commit

--- src/Test/Test2.java
+++ src/Test/Test2.java
@@ -29,12 +29,72 @@ public class Test2 {
 
 		webDriver.navigate().to("http://TheDemoSite.co.uk");
 		
-//		webDriver.findElement(By.cssSelector("#menu > div.navigation-links-hugescreen.visible-f > ul > li:nth-child(2) > div > a > span")).click();
+		webDriver.manage().window().maximize();
+		
+		webDriver.findElement(By.cssSelector("body small a:nth-child(6)")).click();
 		
 		TimeUnit.SECONDS.sleep(3);
-				
 
-			
+
+		webDriver.findElement(By.cssSelector(" table  tbody  tr:nth-child(1)  td:nth-child(2)  p  input")).sendKeys("testuser");
+
+		TimeUnit.SECONDS.sleep(3);
+		
+		
+
+		
+		
+		webDriver.findElement(By.cssSelector(" table  tbody tr:nth-child(2) td:nth-child(2) p  input")).sendKeys("testpass");
+
+		TimeUnit.SECONDS.sleep(3);
+
+		
+
+		
+		
+		
+		//saving password
+		webDriver.findElement(By.cssSelector("table  tbody  tr:nth-child(3) td:nth-child(2) p input")).click();
+		
+		TimeUnit.SECONDS.sleep(3);
+
+
+
+		
+		
+		
+		//clicking login
+
+		webDriver.findElement(By.cssSelector("div table  tbody  tr  td:nth-child(2)  p  small a:nth-child(7)")).click();
+		
+		TimeUnit.SECONDS.sleep(3);	
+
+		
+
+		
+		
+		// Username		
+		webDriver.findElement(By.cssSelector("table  tbody  tr  td:nth-child(1)  table  tbody  tr:nth-child(1)  td:nth-child(2)  p  input")).sendKeys("testuser");
+
+		TimeUnit.SECONDS.sleep(3);
+		
+		
+
+		
+		
+		// User password
+		webDriver.findElement(By.cssSelector("table  tbody tr:nth-child(2)  td:nth-child(2)  p  input")).sendKeys("testpass");
+
+		TimeUnit.SECONDS.sleep(3);
+		
+
+		
+		//saving password
+		webDriver.findElement(By.cssSelector("td.auto-style1  form  div  center  table tbody  tr  td:nth-child(1)  table  tbody  tr:nth-child(3)  td:nth-child(2)  p  input")).click();
+		
+		TimeUnit.SECONDS.sleep(3);
+		
+
 	}
 	
 	
