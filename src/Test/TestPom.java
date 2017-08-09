066ed831936b6e041978dcc75cb0685d8b0371de
ABDULRAOOF003 <ABDULRAOOF003@YAHOO.CO.UK>
2017-08-09 15:22:30 +0100

    commit 4

--- src/Test/TestPom.java
+++ src/Test/TestPom.java
@@ -1,5 +1,6 @@
 package Test;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import com.google.common.base.Function;
 
@@ -78,6 +79,22 @@ public class TestPom {
 	}
 
 	@Test
+	
+	 public void spreadSheet(){
+ 
+
+		SpreadSheetReader sheetReader = new SpreadSheetReader(System.getProperty("user.dir") + "/Example_Spreadsheet.xlsx");
+        List<String> row = sheetReader.readRow(1, "Input Data");
+
+        
+        for(String cell : row){
+            System.out.println(cell);
+        }
+	}
+	
+		
+	
+	
 	public void Test() throws InterruptedException{
 
 	webDriver.navigate().to("http://TheDemoSite.co.uk");
